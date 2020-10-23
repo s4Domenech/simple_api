b387ae0d1411f3b2917315bf019ce5d33f0d1541
Sylvia Domenech <s4domenech@gmail.com>
2020-10-23 23:48:52 +0200

    text changed

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Saludos desde express');
+    res.send('Home');
+});
+
+app.get('/hello', function (req, res) {
+    res.send('hello world');
 });
 
 app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
+    console.log("Initialized 3000 port");
 });
